fc9a7cfbee7cb7300c8d57303729bb01946cf2d7
William Bartos <WBartos@maserconsulting.com>
2016-02-02 13:00:31 -0500

    Added functions to reduce redundancy and added non numerical input functionality

--- stampFromExcel.py
+++ stampFromExcel.py
@@ -5,30 +5,37 @@ import win32com
 import win32com.client
 
 
-totalSDs = int(input('How many shop drawings are being reviewed? '))
+totalSDs = int((input('How many shop drawings are being reviewed? ')))
 print(totalSDs)
 numList = []
 
 
-for i in range(totalSDs):
-        drawingNums = int(input('Enter the Shop Drawing Number: '))
+for i in range((totalSDs)):
+        drawingNums = (input('Enter the Shop Drawing Number: '))
         if drawingNums == '':
             break
         else: 
             numList.append(drawingNums)
+            
+            
 
-        
+def xlsxToPdf(path):
+    xlApp = win32com.client.Dispatch("Excel.Application")
+    books = xlApp.Workbooks.Open(path)
+    ws = books.Worksheets[0]
+    ws.Visible = 1
+    ws.ExportAsFixedFormat(0, path + '.pdf')
 
-
+    
+    
 def stampWriter(numList):
-
-     
+ 
     log = openpyxl.load_workbook(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\ShopDrawingLog.xlsx')
     logSheet = log.get_sheet_by_name('Log')
    
     for i in range(11, logSheet.get_highest_row()+1):
         
-        if logSheet['A' + str(i)].value in numList:
+        if str(logSheet['A' + str(i)].value) in numList:
             
             if logSheet['F' + str(i)].value == 'NET':
                 
@@ -36,13 +43,10 @@ def stampWriter(numList):
                 sheet = wb.get_sheet_by_name('Sheet1')
                 sheet['B1'].value = logSheet['C' + str(i)].value
                 sheet['B2'].value = 'X'
-                wb.save(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
+                sheetPath = (r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i))
+                wb.save(sheetPath + '.xlsx')
                 
-                xlApp = win32com.client.Dispatch("Excel.Application")
-                books = xlApp.Workbooks.Open(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
-                ws = books.Worksheets[0]
-                ws.Visible = 1
-                ws.ExportAsFixedFormat(0, r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.pdf')
+                xlsxToPdf(sheetPath)
                 
             elif logSheet['F' + str(i)].value == 'ET':
                 
@@ -50,13 +54,10 @@ def stampWriter(numList):
                 sheet = wb.get_sheet_by_name('Sheet1')
                 sheet['B1'].value = logSheet['C' + str(i)].value
                 sheet['B3'].value = 'X'
-                wb.save(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
+                sheetPath = (r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i))
+                wb.save(sheetPath + '.xlsx')
                 
-                xlApp = win32com.client.Dispatch("Excel.Application")
-                books = xlApp.Workbooks.Open(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
-                ws = books.Worksheets[0]
-                ws.Visible = 1
-                ws.ExportAsFixedFormat(0, r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.pdf')
+                xlsxToPdf(sheetPath)
                 
             elif logSheet['F' + str(i)].value == 'E&C':
                 
@@ -64,13 +65,10 @@ def stampWriter(numList):
                 sheet = wb.get_sheet_by_name('Sheet1')
                 sheet['B1'].value = logSheet['C' + str(i)].value
                 sheet['B4'].value = 'X'
-                wb.save(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + 'xlsx')
+                sheetPath = (r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i))
+                wb.save(sheetPath + '.xlsx')
                 
-                xlApp = win32com.client.Dispatch("Excel.Application")
-                books = xlApp.Workbooks.Open(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
-                ws = books.Worksheets[0]
-                ws.Visible = 1
-                ws.ExportAsFixedFormat(0, r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.pdf')
+                xlsxToPdf(sheetPath)
                 
             elif logSheet['F' + str(i)].value == 'R&R':
                 
@@ -78,27 +76,21 @@ def stampWriter(numList):
                 sheet = wb.get_sheet_by_name('Sheet1')
                 sheet['B1'].value = logSheet['C' + str(i)].value
                 sheet['B6'].value = 'X'
-                wb.save(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
+                sheetPath = (r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i))
+                wb.save(sheetPath + '.xlsx')
                 
-                xlApp = win32com.client.Dispatch("Excel.Application")
-                books = xlApp.Workbooks.Open(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
-                ws = books.Worksheets[0]
-                ws.Visible = 1
-                ws.ExportAsFixedFormat(0, r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.pdf')
+                xlsxToPdf(sheetPath)
                 
             elif logSheet['F' + str(i)].value == 'REJ':
                 
                 wb = openpyxl.load_workbook(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\Stamp2.xlsx')
                 sheet = wb.get_sheet_by_name('Sheet1')
                 sheet['B1'].value = logSheet['C' + str(i)].value
-                sheet['B6'].value = 'X'
-                wb.save(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
+                sheet['B7'].value = 'X'
+                sheetPath = (r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i))
+                wb.save(sheetPath + '.xlsx')
                 
-                xlApp = win32com.client.Dispatch("Excel.Application")
-                books = xlApp.Workbooks.Open(r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.xlsx')
-                ws = books.Worksheets[0]
-                ws.Visible = 1
-                ws.ExportAsFixedFormat(0, r'C:\Users\wbartos\Documents\Pyscripts\Stamp\newstamp' + str(i) + '.pdf')
+                xlsxToPdf(sheetPath)
             
             
 
