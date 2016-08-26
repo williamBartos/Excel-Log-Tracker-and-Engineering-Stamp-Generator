de18afbd41ec6b8c7956688e63f090539b4a1c54
William Bartos <billybartos@gmail.com>
2016-08-26 16:58:38 -0400

    Bugixes and features

--- transmittalgeneratorGUI.py
+++ transmittalgeneratorGUI.py
@@ -45,77 +45,88 @@ stampDict = {
             
 numList=[]
 
-
+thinBorder = Border(left=Side(style='thin'), 
+                    right=Side(style='thin'), 
+                    top=Side(style='thin'), 
+                    bottom=Side(style='thin')
+                 )
+                 
 def transmittalWriter(numList):
     
-    
-    thinBorder = Border(left=Side(style='thin'), 
-                        right=Side(style='thin'), 
-                        top=Side(style='thin'), 
-                        bottom=Side(style='thin')
-                     )
     
     wbpath = shopDict['Transmittal'] + '.xlsx'
      
     log = openpyxl.load_workbook(shopDict['Log'])
-    logSheet = log.get_sheet_by_name('Log')
-    
+    logSheet = log.get_sheet_by_name('Log')    
     wb = openpyxl.load_workbook(wbpath)
     sheet = wb.get_sheet_by_name('Sheet1')
-    
-    
-    
+        
     currentRow = 29 #STARTING ROW FOR SUBMITTAL TABLE
     headerRow = currentRow - 1
+
+    def copyValues(currentRow):
+        
+        sheet['E13'].value = logSheet['A3'].value #Project Name
+        sheet['E14'].value = logSheet['A4'].value #Client Name
+        sheet['G11'].value = logSheet['A5'].value #MC Project No.
+        sheet['A'+str(currentRow)].value = str(logSheet['A' + str(logrow)].value) #SD NO. 
+        sheet['C'+str(currentRow)].value = str(logSheet['C' + str(logrow)].value) #DESCRIPTION
+        sheet['H'+str(currentRow)].value = str(logSheet['F' + str(logrow)].value) #STATUS
+        
+        sheet['A'+str(currentRow)].border = thinBorder
+        sheet['B'+str(currentRow)].border = thinBorder
+        sheet['C'+str(currentRow)].border = thinBorder
+        sheet['D'+str(currentRow)].border = thinBorder
+        sheet['E'+str(currentRow)].border = thinBorder
+        sheet['F'+str(currentRow)].border = thinBorder
+        sheet['G'+str(currentRow)].border = thinBorder
+        sheet['H'+str(currentRow)].border = thinBorder
+    
+        sheet['A'+str(headerRow)].border = thinBorder
+        sheet['B'+str(headerRow)].border = thinBorder
+        sheet['C'+str(headerRow)].border = thinBorder
+        sheet['D'+str(headerRow)].border = thinBorder
+        sheet['E'+str(headerRow)].border = thinBorder
+        sheet['F'+str(headerRow)].border = thinBorder
+        sheet['G'+str(headerRow)].border = thinBorder
+        sheet['H'+str(headerRow)].border = thinBorder
+
+    
       
     for logrow in range(11, logSheet.get_highest_row()+1):
         if str(logSheet['A' + str(logrow)].value) in numList:
-            pdfFileObj = open(logSheet['K'+str(logrow)].value, 'rb')
-            pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-            
-            sheet['E13'].value = logSheet['A3'].value #Project Name
-            sheet['E14'].value = logSheet['A4'].value #Client Name
-            sheet['G11'].value = logSheet['A5'].value #MC Project No.
-            sheet['A'+str(currentRow)].value = str(logSheet['A' + str(logrow)].value) #SD NO. 
-            sheet['B'+str(currentRow)].value = pdfReader.numPages #TOTAL PAGES 
-            sheet['C'+str(currentRow)].value = str(logSheet['C' + str(logrow)].value) #DESCRIPTION
-            sheet['H'+str(currentRow)].value = str(logSheet['F' + str(logrow)].value) #STATUS
-            
-            sheet['A'+str(currentRow)].border = thinBorder
-            sheet['B'+str(currentRow)].border = thinBorder
-            sheet['C'+str(currentRow)].border = thinBorder
-            sheet['D'+str(currentRow)].border = thinBorder
-            sheet['E'+str(currentRow)].border = thinBorder
-            sheet['F'+str(currentRow)].border = thinBorder
-            sheet['G'+str(currentRow)].border = thinBorder
-            sheet['H'+str(currentRow)].border = thinBorder
-            #sheet['I'+str(currentRow)].border = thinBorder
-            #sheet['J'+str(currentRow)].border = thinBorder
-            
-            sheet['A'+str(headerRow)].border = thinBorder
-            sheet['B'+str(headerRow)].border = thinBorder
-            sheet['C'+str(headerRow)].border = thinBorder
-            sheet['D'+str(headerRow)].border = thinBorder
-            sheet['E'+str(headerRow)].border = thinBorder
-            sheet['F'+str(headerRow)].border = thinBorder
-            sheet['G'+str(headerRow)].border = thinBorder
-            sheet['H'+str(headerRow)].border = thinBorder
-            #sheet['I'+str(headerRow)].border = thinBorder
-            #sheet['J'+str(headerRow)].border = thinBorder
-            
-            
-            
-            currentRow+= 1
-    
-    newTransmittal = ((shopDict['Out'] + '\\Transmittal'))
-    wb.save(newTransmittal)
-
-    xlsxToPdf(newTransmittal)
-    addHeader(newTransmittal + '.pdf') 
-    
-    os.remove(newTransmittal)
-    os.remove(newTransmittal + '.pdf')
+            try:
+                pdfFileObj = open(logSheet['K'+str(logrow)].value, 'rb')
+                pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+                copyValues(currentRow)
                 
+            except:
+                copyValues(currentRow)
+                currentRow+= 1
+                 
+                try:
+                    sheet['B'+str(currentRow)].value = pdfReader.numPages #TOTAL PAGES 
+                except:
+                    continue
+                       
+        currentRow+= 1
+                
+    try:
+        newTransmittal = ((shopDict['Out'] + '\\Transmittal'))
+        wb.save(newTransmittal)
+    
+        xlsxToPdf(newTransmittal)
+        addHeader(newTransmittal + '.pdf') 
+        
+        os.remove(newTransmittal)
+        os.remove(newTransmittal + '.pdf')
+        
+    except:
+        os.remove(newTransmittal)
+        os.remove(newTransmittal + '.pdf')
+                
+
+
 
 def xlsxToPdf(path):
     xlApp = win32com.client.Dispatch("Excel.Application")
